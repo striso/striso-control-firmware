49899dfab4e2841660970be8d166623b7ae2169c
Piers Titus van der Torren <pierstitus@gmail.com>
2015-02-14 09:06:45 +0100

    fix multi adc mode as suggested by Giovanni

--- adc_multi.c
+++ adc_multi.c
@@ -168,7 +168,7 @@ void adc_multi_lld_start_conversion(ADCDriver *adcp) {
   /* ADC configuration and start, the start is performed using the method
      specified in the CR2 configuration, usually ADC_CR2_SWSTART.*/
   if ((grpp->cr2 & ADC_CR2_SWSTART) != 0) {
-    adcp->adc->CR2 = (grpp->cr2 & !ADC_CR2_SWSTART) | ADC_CR2_ADON | ADC_CR2_CONT;
+    adcp->adc->CR2 = (grpp->cr2 & ~ADC_CR2_SWSTART) | ADC_CR2_ADON | ADC_CR2_CONT;
     // Setting SWSTART in a separate write prevents spurious DMA request
     adcp->adc->CR2 |= ADC_CR2_SWSTART;
   }
