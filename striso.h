e430a61abfc3975788d96acad33582cb5f06f97c
Piers Titus van der Torren <pierstitus@gmail.com>
2017-07-29 15:50:36 +0200

    binary protocol update

--- striso.h
+++ striso.h
@@ -9,20 +9,22 @@
 #define ID_SYS 4      // system, status, debug
 
 // control numbers
-#define IDC_PORTAMENTO 0
-#define IDC_OCT_UP 1
-#define IDC_OCT_DOWN 2
-#define IDC_ALT 3
-#define IDC_SLD_NPRESS 8
-#define IDC_SLD_SLIDE 9
-#define IDC_SLD_SLIDEZOOM 10
-#define IDC_SLD_ONE_TAP 3
-#define IDC_SLD_TWO_TAP 4
+#define IDC_PORTAMENTO 0      // length 1
+#define IDC_OCT_UP 1          // length 1
+#define IDC_OCT_DOWN 2        // length 1
+#define IDC_ALT 3             // length 1
+#define IDC_SLD_NPRESS 8      // length 1
+#define IDC_SLD_SLIDE 9       // length 1
+#define IDC_SLD_SLIDEZOOM 10  // length 2
+#define IDC_SLD_ONE_TAP 3     // length 1
+#define IDC_SLD_TWO_TAP 4     // length 1
 
 // system numbers
 #define ID_SYS_MSGQUE_OVERFLOW_BB 1
 #define ID_SYS_MSGQUE_OVERFLOW_SYNTH 2
 #define ID_SYS_MSG_TOO_SHORT_SYNTH 3
 #define ID_SYS_MSG_TOO_LONG_SYNTH 4
+#define ID_SYS_BATTERY_VOLTAGE 5
+#define ID_SYS_TEMPERATURE 6
 
 #endif
